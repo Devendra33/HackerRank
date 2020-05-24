70a7cb2fc4f1322231d0dc810e0f37a59ec52daf
Devendra33 <47396740+Devendra33@users.noreply.github.com>
2020-05-25 00:14:39 +0530

    Delete set.unioin.py

--- set.unioin.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = input()
-n_inp = set(input().split())
-b = input()
-b_inp = set(input().split())
-
-res = n_inp.union(b_inp)
-print(len(res))
