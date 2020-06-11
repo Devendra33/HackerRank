7577942f7854e53fad219365c9456da3c57eabb5
Devendra33 <47396740+Devendra33@users.noreply.github.com>
2020-06-11 11:30:52 +0530

    Add files via upload

---
+++ Problem Solving/Extra Long Factorial.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+from math import factorial
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    print(factorial(n))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    extraLongFactorials(n)
