b8fa99ab760921370dc3d2a7cb0eef455c895cf9
Devendra Gohare <47396740+Devendra33@users.noreply.github.com>
2020-09-12 14:53:29 +0530

    Add files via upload

---
+++ Problem Solving/Library fine.py
@@ -0,0 +1,16 @@
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    if y1 <= y2 and m1 < m2:
+        return 0
+    if y1 > y2:
+        return 10000
+    else:
+        if y1 == y2:
+            if m1 <= m2:
+                if d1 <= d2:
+                    return 0
+                else:
+                    return 15 * (d1 - d2)
+            else:
+                return 500 * (m1 - m2)
+        else:
+            return 0
